8de95956f167027975a9959bc971ba0965aabfac
Jan Maaß <git@antipost.de>
2023-12-06 16:26:30 +0100

    Add recipe frontend

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import useSWR from "swr";
+import Link from "next/link";
+import fetcher from "@/lib/fetcher";
+
 export default function HomePage() {
+  const { data: recipes, mutate } = useSWR("/api/recipes", fetcher);
+
+  if (!recipes) return <h2>Is loading...</h2>;
+
   return (
     <div>
-      <h1>Hello Next!</h1>
+      <h1>Recipes</h1>
+      {recipes.length ? (
+        <ul>
+          {recipes.map(({ _id, name, description, ingredients, instructions }) => (
+            <li key={_id}>
+              <strong>{name}</strong>
+              <p>{description}</p>
+              <p>Ingredients:
+                <ul>{ingredients.map(ingredient => <li>{ingredient.quantity} {ingredient.unit} {ingredient.name}</li>)}</ul></p>
+              <p>Instructions: {instructions}</p>
+              <Link href={`/recipes/${_id}`}>Read More</Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        "No services yet."
+      )}
     </div>
   );
 }
